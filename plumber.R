088ccea86a3046d1eb1c5d64a158b64e20801fbc
gitlexander <astavro123@gmail.com>
2024-07-03 16:14:43 -0400

    Update plumber.R

--- plumber.R
+++ plumber.R
@@ -9,5 +9,5 @@ function(x, y, z){
   x <- as.numeric(x)
   y <- as.numeric(y)
   z <- as.numeric(z)  
-response = c(all = x+y+z,some = x+y,some2 = z+y,some3 = x+z)
+list(response = list(all = x+y+z,some = x+y,some2 = z+y,some3 = x+z))
 }
