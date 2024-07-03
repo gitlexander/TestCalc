eb3301f17257160131576e1d1ccee7c907ad49ae
gitlexander <astavro123@gmail.com>
2024-07-03 14:00:04 -0400

    Create plumber.R

---
+++ plumber.R
@@ -0,0 +1,13 @@
+library(plumber)
+
+#* get the sum of variables as defined by Bubble data
+#* @param x gender
+#* @param y med number
+#* @param z med history number
+#* @get /output
+function(x, y, z){
+  x <- as.numeric(x)
+  y <- as.numeric(y)
+  z <- as.numeric(z)  
+    x+y+z
+}
