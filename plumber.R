5bbb0238387951cdf4a7593b1775489b6912b6ba
gitlexander <astavro123@gmail.com>
2024-07-08 10:41:54 -0400

    Update plumber.R

--- plumber.R
+++ plumber.R
@@ -1,13 +1,24 @@
 library(plumber)
 
+#* get the list of subtasks
+#* @get /subtasks
+
+function(){
+ranked_subtask = list("I3", "M1", "I1", "G1", "A1", "R1", "G2", "B4", "A4", "T1", "T2", "D2", "A2", "A6", "H1", "D5", "D4", "G3", "R2", "D3", "B2", "C1", "C2", "P1", "A5", "B1", "B3", "D6")
+ }
+
+if(FALSE){ 
 #* get the sum of variables as defined by Bubble data
 #* @param x gender
 #* @param y med number
 #* @param z med history number
 #* @get /output
+  
 function(x, y, z){
   x <- as.numeric(x)
   y <- as.numeric(y)
   z <- as.numeric(z)  
 list(all = x+y+z,some = x+y,some2 = z+y,some3 = x+z)
+ }
+  
 }
