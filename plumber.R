4f34b7e630c1c37bc27ae38bf228ff8bedafda85
gitlexander <astavro123@gmail.com>
2024-07-08 16:49:39 -0400

    Update plumber.R

--- plumber.R
+++ plumber.R
@@ -3,7 +3,7 @@ library(plumber)
 #* get the list of subtasks
 #* @get /subtasks
 function() {
-  ranked_subtask = list(subtasks = c(I3 = "I3", M1 = "M1", I1 = "I1", G1 = "G1", A1 = "A1", R1 = "R1", G2 = "G2", B4 = "B4", A4 = "A4", T1 = "T1", T2 = "T2", D2 = "D2", A2 = "A2", A6 = "A6", H1 = "H1", D5 = "D5", D4 = "D4", G3 = "G3", R2 = "R2", D3 = "D3", B2 = "B2", C1 = "C1", C2 = "C2", P1 = "P1", A5 = "A5", B1 = "B1", B3 = "B3", D6 = "D6"
+  ranked_subtask = c(I3 = "I3", M1 = "M1", I1 = "I1", G1 = "G1", A1 = "A1", R1 = "R1", G2 = "G2", B4 = "B4", A4 = "A4", T1 = "T1", T2 = "T2", D2 = "D2", A2 = "A2", A6 = "A6", H1 = "H1", D5 = "D5", D4 = "D4", G3 = "G3", R2 = "R2", D3 = "D3", B2 = "B2", C1 = "C1", C2 = "C2", P1 = "P1", A5 = "A5", B1 = "B1", B3 = "B3", D6 = "D6"
 ))
 }
 
