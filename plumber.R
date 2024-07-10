93848af395dd8065524061a45dd33d45a2c34dc2
gitlexander <astavro123@gmail.com>
2024-07-10 09:49:29 -0400

    Update plumber.R

--- plumber.R
+++ plumber.R
@@ -1,24 +1,41 @@
 library(plumber)
 
-#* get the list of subtasks
+#* Get the list of subtasks
 #* @get /subtasks
 function() {
-  ranked_subtask = c(I3 = "I3", M1 = "M1", I1 = "I1", G1 = "G1", A1 = "A1", R1 = "R1", G2 = "G2", B4 = "B4", A4 = "A4", T1 = "T1", T2 = "T2", D2 = "D2", A2 = "A2", A6 = "A6", H1 = "H1", D5 = "D5", D4 = "D4", G3 = "G3", R2 = "R2", D3 = "D3", B2 = "B2", C1 = "C1", C2 = "C2", P1 = "P1", A5 = "A5", B1 = "B1", B3 = "B3", D6 = "D6"
-))
-}
-
-if(FALSE){ 
-#* get the sum of variables as defined by Bubble data
-#* @param x gender
-#* @param y med number
-#* @param z med history number
-#* @get /output
-  
-function(x, y, z){
-  x <- as.numeric(x)
-  y <- as.numeric(y)
-  z <- as.numeric(z)  
-list(all = x+y+z,some = x+y,some2 = z+y,some3 = x+z)
- }
-  
-}
+  res <- list(
+    response = list(
+      results = list(
+        list(subID = "I3", subprob = 0.54),
+        list(subID = "M1", subprob = 0.67),
+        list(subID = "I1", subprob = 0.43),
+        list(subID = "G1", subprob = 0.75),
+        list(subID = "A1", subprob = 0.59),
+        list(subID = "R1", subprob = 0.62),
+        list(subID = "G2", subprob = 0.48),
+        list(subID = "B4", subprob = 0.77),
+        list(subID = "A4", subprob = 0.56),
+        list(subID = "T1", subprob = 0.68),
+        list(subID = "T2", subprob = 0.64),
+        list(subID = "D2", subprob = 0.52),
+        list(subID = "A2", subprob = 0.58),
+        list(subID = "A6", subprob = 0.49),
+        list(subID = "H1", subprob = 0.61),
+        list(subID = "D5", subprob = 0.53),
+        list(subID = "D4", subprob = 0.57),
+        list(subID = "G3", subprob = 0.69),
+        list(subID = "R2", subprob = 0.65),
+        list(subID = "D3", subprob = 0.50),
+        list(subID = "B2", subprob = 0.66),
+        list(subID = "C1", subprob = 0.60),
+        list(subID = "C2", subprob = 0.55),
+        list(subID = "P1", subprob = 0.72),
+        list(subID = "A5", subprob = 0.51),
+        list(subID = "B1", subprob = 0.70),
+        list(subID = "B3", subprob = 0.63),
+        list(subID = "D6", subprob = 0.71)
+      )
+    )
+  )
+  return(res)
+  }
